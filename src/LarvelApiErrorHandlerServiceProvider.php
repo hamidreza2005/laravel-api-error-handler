a0282bc761fe878b044c591ac93fda63258047a6
Hamidreza Hassani <h.r.hassani2005@gmail.com>
2020-10-21 11:41:39 +0330

    update composer.json

---
+++ src/LarvelApiErrorHandlerServiceProvider.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace hamidreza2005\laravelApiErrorHandler;
+
+use Illuminate\Support\ServiceProvider;
+
+class LaravelApiErrorHandlerServiceProvider extends ServiceProvider{
+    
+    public function register()
+    {
+        
+    }
+
+    public function boot()
+    {
+        
+    }
+}
