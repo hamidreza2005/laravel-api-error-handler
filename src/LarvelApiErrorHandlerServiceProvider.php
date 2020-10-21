9dd892087360bb0e4666dcf3bb1ab7f43056f500
Hamidreza Hassani <h.r.hassani2005@gmail.com>
2020-10-21 11:48:52 +0330

    added listener and service provider and config files

--- src/LarvelApiErrorHandlerServiceProvider.php
+++ src/LarvelApiErrorHandlerServiceProvider.php
@@ -1,18 +1,25 @@
 <?php
 
-namespace hamidreza2005\laravelApiErrorHandler;
+namespace hamidreza2005\LaravelApiErrorHandler;
 
+use hamidreza2005\LaravelApiErrorHandler\Listeners\ErrorListener;
+use Illuminate\Foundation\Http\Events\RequestHandled;
+use Illuminate\Support\Facades\Event;
 use Illuminate\Support\ServiceProvider;
 
 class LaravelApiErrorHandlerServiceProvider extends ServiceProvider{
-    
+
     public function register()
     {
-        
+        $this->mergeConfigFrom(__DIR__.'/Config/api-error-handler.php','api-error-handler');
     }
 
     public function boot()
     {
-        
+        $this->publishes([
+            __DIR__.'/Config/api-error-handler.php'=>config_path('api-error-handler.php')
+        ],'laravel-api-error-handler');
+
+        Event::listen(RequestHandled::class,ErrorListener::class);
     }
 }
---
+++ src/Listeners/ErrorListener.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace hamidreza2005\LaravelApiErrorHandler\Listeners;
+
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Queue\InteractsWithQueue;
+
+class ErrorListener
+{
+    /**
+     * Create the event listener.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Handle the event.
+     *
+     * @param  object  $event
+     * @return void
+     */
+    public function handle($event)
+    {
+        //
+    }
+}
