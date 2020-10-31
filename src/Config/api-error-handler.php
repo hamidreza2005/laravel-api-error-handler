d0f1fd6672025a2b913fa681137d35938501fa1a
Hamidreza Hassani <h.r.hassani2005@gmail.com>
2020-10-31 10:26:28 +0330

    Added MethodNotAllowedException to the configs

--- src/Config/api-error-handler.php
+++ src/Config/api-error-handler.php
@@ -7,4 +7,5 @@ return [
     "Illuminate\Auth\AuthenticationException" => "\hamidreza2005\LaravelApiErrorHandler\Exceptions\AccessDeniedException",
     "Symfony\Component\HttpKernel\Exception\HttpException" => "\hamidreza2005\LaravelApiErrorHandler\Exceptions\AccessDeniedException",
     "Illuminate\Validation\ValidationException" => "\hamidreza2005\LaravelApiErrorHandler\Exceptions\ValidationException",
+    "Symfony\Component\HttpKernel\Exception\MethodNotAllowedHttpException"=>"\hamidreza2005\LaravelApiErrorHandler\Exceptions\NotFoundException",
 ];
