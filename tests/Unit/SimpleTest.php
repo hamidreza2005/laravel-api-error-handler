b778eb71b3582ce304041150bf824dee7503843d
Hamidreza Hassani <h.r.hassani2005@gmail.com>
2023-10-13 12:44:53 +0330

    add tests and support for laravel 10

---
+++ tests/Unit/SimpleTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace hamidreza2005\LaravelApiErrorHandler\Tests\Unit;
+
+use Orchestra\Testbench\TestCase;
+
+class SimpleTest extends TestCase
+{
+    public function test_true_is_true()
+    {
+        $this->assertTrue(true);
+    }
+}
