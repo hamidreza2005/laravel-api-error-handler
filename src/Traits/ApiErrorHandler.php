a98894c3748be8dd87ebfa5d159bc21a3596fd7e
Hamidreza Hassani <h.r.hassani2005@gmail.com>
2023-10-13 12:30:28 +0330

    refactor

--- src/Traits/ApiErrorHandler.php
+++ src/Traits/ApiErrorHandler.php
@@ -4,17 +4,26 @@ namespace hamidreza2005\LaravelApiErrorHandler\Traits;
 
 use hamidreza2005\LaravelApiErrorHandler\Exceptions\DefaultException;
 use hamidreza2005\LaravelApiErrorHandler\Exceptions\ServerInternalException;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Support\Facades\Response;
 
 trait ApiErrorHandler
 {
-    public function handleError($exception)
+    public function handleError($exception): JsonResponse
     {
         $exceptions = config("api-error-handler") ?? [];
-        $class = array_key_exists(get_class($exception),$exceptions) ? $exceptions[get_class($exception)] : (config('app.debug') ? DefaultException::class : ServerInternalException::class );
+        $class = array_key_exists(get_class($exception),$exceptions) ?
+            $exceptions[get_class($exception)] :
+            (config('app.debug') ? DefaultException::class : ServerInternalException::class
+        );
         $handler = new $class($exception);
         $handler->handleStatusCode();
         $handler->handleMessage();
-        return Response::json(["error"=>$handler->getMessage()],$handler->getStatusCode(),["Content-Type"=>"application/json"]);
+        return $this->errorResponse(["error"=>$handler->getMessage()],$handler->getStatusCode(),["Content-Type"=>"application/json"]);
+    }
+
+    protected function errorResponse(array $data,int $statusCode, array $headers): JsonResponse
+    {
+        return Response::json($data,$statusCode,$headers);
     }
 }
